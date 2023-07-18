ddade9e4adad8a82e6c8c35b46b5d0e62f9954e6
Clinton Brito <clintoncsb@gmail.com>
2023-07-18 15:49:34 -0300

    :sparkles: [Req. 3] feat: challenge ok

---
+++ challenges/desafio3.sql
@@ -0,0 +1,14 @@
+SELECT
+	u.user_name AS pessoa_usuaria,
+    COUNT(ph.user_id) AS musicas_ouvidas,
+    ROUND(SUM(s.duration_seconds / 60), 2) AS total_minutos
+FROM
+	SpotifyClone.users AS u
+		INNER JOIN
+	SpotifyClone.playback_history AS ph ON ph.user_id = u.user_id
+		INNER JOIN
+	SpotifyClone.songs AS s ON s.song_id = ph.song_id
+GROUP BY
+	u.user_name
+ORDER BY
+	u.user_name ASC;
